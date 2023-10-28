e21e2b581f9471212b7cbf44651a8b29410aa768
Andreea <63120475+ezra0619@users.noreply.github.com>
2023-10-28 12:21:00 +0200

    pushing day 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AocDay1Component } from './aoc-2021/aoc-day1/aoc-day1.component';
 import {AocDay2Component} from "./aoc-2021/aoc-day2/aoc-day2.component";
-import {AocDay3Component} from "./aoc-2021/aoc-day-3/aoc-day3.component";
+import {AocDay3Component} from "./aoc-2021/aoc-day3/aoc-day3.component";
 
 @NgModule({
     declarations: [
